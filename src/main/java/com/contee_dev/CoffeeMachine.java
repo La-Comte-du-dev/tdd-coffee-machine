e4fa57a3632a3d5332fd90747512ae15eb46927c
BenoitDeligny <delignyb.pro@gmail.com>
2024-10-21 21:58:55 +0200

    add calcified rule

--- src/main/java/com/contee_dev/CoffeeMachine.java
+++ src/main/java/com/contee_dev/CoffeeMachine.java
@@ -4,22 +4,49 @@ public class CoffeeMachine {
     // Todo : create dedicated "reservoir" class
     private final int MIN_WATER_LVL = 20;
     private final int MAX_WATER_LVL = 100;
+    private final int CALCIFIED_THRESHOLD = 40;
 
     private boolean isSwitchedOn;
     private int waterLvl;
+    private int calcificationPercent;
 
     public CoffeeMachine() {
         this.isSwitchedOn = false;
         this.waterLvl = 0;
+        this.calcificationPercent = 0;
     }
 
-    // Business logic
+    public CoffeeMachine(int calcificationPercent) {
+        this.isSwitchedOn = false;
+        this.waterLvl = 0;
+        this.calcificationPercent = calcificationPercent;
+    }
+
     public void fillWater(int quantityInCL) {
         this.waterLvl = quantityInCL;
     }
 
+    public void makeACoffee() {
+        if (canWork()) {
+            this.waterLvl -= 10;
+            this.calcificationPercent += 5;
+        }
+    }
+
     public boolean canWork() {
+        return notCalcified() && enoughWater();
+    }
+
+    private boolean enoughWater() {
         return this.waterLvl > MIN_WATER_LVL && this.waterLvl <= MAX_WATER_LVL;
+    }
+
+    private boolean notCalcified() {
+        return this.calcificationPercent < CALCIFIED_THRESHOLD;
+    }
+
+    public int calcificationPercent() {
+        return calcificationPercent;
     }
 
     // Getters & Setters de mort
@@ -34,6 +61,4 @@ public class CoffeeMachine {
     public void switchOff() {
         this.isSwitchedOn = false;
     }
-
-
 }
--- src/test/java/com/contee_dev/CoffeeMachineTest.java
+++ src/test/java/com/contee_dev/CoffeeMachineTest.java
@@ -5,59 +5,84 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class CoffeeMachineTest{
-
-   @Test
-   public void shouldReturnTrue_IfMachineIsSwitchedOn() {
-       // given (mes inputs)
-       CoffeeMachine machine = new CoffeeMachine();
-       // when (ce qu'il se passe)
-       machine.switchOn();
-       boolean isSwitchedOn = machine.isSwitchedOn();
-       // then (assertions / ce que j'attends / l'output)
-       assertEquals(true, isSwitchedOn);
-   }
-
-   @Test
-   public void shouldReturnFalse_IfMachineIsSwitchedOff() {
-       CoffeeMachine machine = new CoffeeMachine();
-       machine.switchOff();
-       boolean isSwitchedOn = machine.isSwitchedOn();
-       assertEquals(false, isSwitchedOn);
-   }
-
-   @Test
-   public void canMachineCreateCoffee_isWaterLevelIsGreaterThan20CL() {
-    // given
-    CoffeeMachine machine = new CoffeeMachine();
-    machine.fillWater(21);
-    // when
-    boolean canMachineCreateCoffee = machine.canWork();
-    // then
-    assertEquals(true, canMachineCreateCoffee);
-   }
-
-   @Test
-   public void cannotMachineCreateCoffee_isWaterLevelIsEqualOrMinusThan20CL() {
-    // given
-    CoffeeMachine machine = new CoffeeMachine();
-    machine.fillWater(20);
-    // when
-    boolean canMachineCreateCoffee = machine.canWork();
-    // then
-    assertEquals(false, canMachineCreateCoffee);
-   }
+public class CoffeeMachineTest {
 
     @Test
-    public void cannotMachineCreateCoffee_isWaterLevelIsGreaterThan100CL() {
+    public void shouldReturnTrue_IfMachineIsSwitchedOn() {
+        // given (mes inputs)
+        CoffeeMachine machine = coffeeMachineWith(0, 0);
+        // when (ce qu'il se passe)
+        machine.switchOn();
+        boolean isSwitchedOn = machine.isSwitchedOn();
+        // then (assertions / ce que j'attends / l'output)
+        assertEquals(true, isSwitchedOn);
+    }
+
+    @Test
+    public void shouldReturnFalse_IfMachineIsSwitchedOff() {
+        CoffeeMachine machine = coffeeMachineWith(0, 0);
+        machine.switchOff();
+        boolean isSwitchedOn = machine.isSwitchedOn();
+        assertEquals(false, isSwitchedOn);
+    }
+
+    @Test
+    public void canMachineCreateCoffee_isWaterLevelIsGreaterThan20CL() {
         // given
-        CoffeeMachine machine = new CoffeeMachine();
-        machine.fillWater(101);
+        CoffeeMachine machine = coffeeMachineWith(21, 0);
+        // when
+        boolean canMachineCreateCoffee = machine.canWork();
+        // then
+        assertEquals(true, canMachineCreateCoffee);
+    }
+
+    @Test
+    public void cannotMachineCreateCoffee_isWaterLevelIsEqualOrMinusThan20CL() {
+        // given
+        CoffeeMachine machine = coffeeMachineWith(20, 0);
         // when
         boolean canMachineCreateCoffee = machine.canWork();
         // then
         assertEquals(false, canMachineCreateCoffee);
     }
 
+    @Test
+    public void cannotMachineCreateCoffee_isWaterLevelIsGreaterThan100CL() {
+        // given
+        CoffeeMachine machine = coffeeMachineWith(101, 0);
+        // when
+        boolean canMachineCreateCoffee = machine.canWork();
+        // then
+        assertEquals(false, canMachineCreateCoffee);
+    }
 
+    @Test
+    public void CannotMakeACoffee_WhenCoffeeMachineNotCalcified() {
+        // given
+        CoffeeMachine machine = coffeeMachineWith(21, 45);
+
+        // when
+        // then
+        assertEquals(false, machine.canWork());
+    }
+
+    @Test
+    public void ShouldIncrementCalcification_WhenMachineIsUsed() {
+        // given
+        CoffeeMachine machine = coffeeMachineWith(21, 0);
+
+        // when
+        machine.makeACoffee();
+
+        // then
+        assertEquals(5, machine.calcificationPercent());
+    }
+
+    // PILOU (OR ANOTHER PERSON) PLEASE MAKE A BUILDER HERE
+    private CoffeeMachine coffeeMachineWith(int waterLvl, int calcificationPercent) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine(calcificationPercent);
+        coffeeMachine.fillWater(waterLvl);
+
+        return coffeeMachine;
+    }
 }
